b0b448a09a55bb21ffdaba23e2d3d8f5e7d933ba
rdxotpbotshop-rgb <rdxotpbotshop@gmail.com>
2025-11-25 18:24:36 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/my-v0-project' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-v0-project/' : '',
 }
 
 export default nextConfig
